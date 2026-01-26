db78c0ac244f883d0c5618d21b68d86c854b4e6d
Ryan <eklesp@icloud.com>
2026-01-26 11:49:42 -0800

    service worker

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE = 'nulkey-v3';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.add('.'))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
